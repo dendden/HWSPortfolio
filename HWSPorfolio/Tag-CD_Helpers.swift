a421fc2dd691b9a0fdbe4f7bc314a31ab530280f
Denys Triasunov <dendden@users.noreply.github.com>
2023-04-19 15:31:14 +0300

    Added uncommited changes

--- HWSPorfolio/Tag-CD_Helpers.swift
+++ HWSPorfolio/Tag-CD_Helpers.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+extension Tag {
+    
+    var tagID: UUID {
+        self.id ?? UUID()
+    }
+    
+    var tagName: String {
+        self.name ?? ""
+    }
+    
+    var tagActiveIssues: [Issue] {
+        let result = self.issues?.allObjects as? [Issue] ?? []
+        return result.filter { !$0.completed }
+    }
+    
+    static var example: Tag {
+        
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let tag = Tag(context: viewContext)
+        tag.id = UUID()
+        tag.name = "Example Tag"
+        
+        return tag
+    }
+}
+
+extension Tag: Comparable {
+    
+    public static func <(lhs: Tag, rhs: Tag) -> Bool {
+        
+        let left = lhs.tagName.localizedLowercase
+        let right = rhs.tagName.localizedLowercase
+        
+        if left != right {
+            return left < right
+        } else {
+            return lhs.tagID.uuidString < rhs.tagID.uuidString
+        }
+    }
+}
