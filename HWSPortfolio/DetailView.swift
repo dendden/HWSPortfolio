b524369a9d0d71f85527e2eea8d957dfee974cb6
Denys Triasunov <dendden@users.noreply.github.com>
2023-04-20 19:46:56 +0300

    (Temp) add View extension to support or ignore inline NavBar title depending on running OS

--- HWSPortfolio/DetailView.swift
+++ HWSPortfolio/DetailView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+extension View {
+    func multiPlatformInlineNavigationBarTitle() -> some View {
+        #if os(iOS)
+        self.navigationBarTitleDisplayMode(.inline)
+        #else
+        self
+        #endif
+    }
+}
+
 struct DetailView: View {
     
     @EnvironmentObject var dataController: DataController
@@ -20,7 +30,7 @@ struct DetailView: View {
             }
         }
         .navigationTitle("Details")
-        .navigationBarTitleDisplayMode(.inline)
+        .multiPlatformInlineNavigationBarTitle()
     }
 }
 
